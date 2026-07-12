0a44a7b472cdc68ae237c92e33ffab4ed90648ea
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-07-12 14:16:20 +0200

    feat: initial

--- src/main.c
+++ src/main.c
@@ -1,6 +1,65 @@
 #include "main.h"
 
-i32 main(i32 argc, char **argv) {
-    printf("%s\n", ENV_NAME);
+void err(const char *msg) {
+    printf("Error: %s", msg);
+    exit(1);
+}
+
+char *get_home_dir() {
+    char buff[256] = {0};
+    char *d = getcwd(buff, 256);
+    int brack_encount = 0;
+    char *part1 = (char *)malloc(256);
+    for (int i = 0; d[i] != '\0'; i++) {
+        if (d[i] == '/') {
+            brack_encount++;
+        }
+        if (brack_encount == 3) {
+            strncpy(part1, d, i);
+            part1[i] = '\0';
+            break;
+        }
+    }
+    return part1;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        err("no arguments were provided");
+    }
+
+    // get markdown file from args and resolve home directory
+    char md_path[256] = {0};
+    if (argv[1][0] == '~') {
+        char *h = get_home_dir();
+        char *ptr = argv[1];
+        ptr++;
+        snprintf(md_path, 255, "%s%s", h, ptr);
+        free(h);
+    } else {
+        strcpy(md_path, argv[1]);
+    }
+    md_path[255] = '\0';
+    if (!BUILD_RELEASE) {
+        printf("File at path: %s\n", md_path);
+    }
+
+    FILE *file = fopen(md_path, "r");
+    if (file == NULL) {
+        err("failed to open file\n");
+        return 1;
+    }
+
+    char buffer[2048];
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("%s", buffer);
+    }
+
+    if (feof(file)) {
+    } else if (ferror(file)) {
+        printf("An error occurred.\n");
+    }
+
+    fclose(file);
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -17,6 +17,7 @@
 
 // standard libraries ---------------------------------------------------------
 
+#include <libc.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -68,31 +69,31 @@ typedef double f64;
 #endif
 
 static inline void _log_template(const char *file, size_t file_len, i32 line,
-        const char *func, size_t func_len, char *type,
-        const char *fmt, ...) {
-    char msg[256];
+                                 const char *func, size_t func_len, char *type,
+                                 const char *fmt, ...) {
+  char msg[256];
 
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(msg, sizeof(msg), fmt, args);
-    va_end(args);
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(msg, sizeof(msg), fmt, args);
+  va_end(args);
 
-    char file_name[24];
-    if (file_len > 11) {
-        snprintf(file_name, sizeof(file_name), "%.10s…", file);
-    } else {
-        strcpy(file_name, file);
-    }
+  char file_name[24];
+  if (file_len > 11) {
+    snprintf(file_name, sizeof(file_name), "%.10s…", file);
+  } else {
+    strcpy(file_name, file);
+  }
 
-    char fn_name[24];
-    if (func_len > 11) {
-        snprintf(fn_name, sizeof(fn_name), "%.10s…", func);
-    } else {
-        strcpy(fn_name, func);
-    }
+  char fn_name[24];
+  if (func_len > 11) {
+    snprintf(fn_name, sizeof(fn_name), "%.10s…", func);
+  } else {
+    strcpy(fn_name, func);
+  }
 
-    printf("\033[1m%-8s\033[0m ┆ %-11s ┆ %-4d ┆ %-11s ┆ %s\n", type, file_name,
-            line, fn_name, msg);
+  printf("\033[1m%-8s\033[0m ┆ %-11s ┆ %-4d ┆ %-11s ┆ %s\n", type, file_name,
+         line, fn_name, msg);
 }
 
 #if defined(NDEBUG)
@@ -100,41 +101,41 @@ static inline void _log_template(const char *file, size_t file_len, i32 line,
 #define LOG(fmt, ...) ((void)0)
 #else
 #define ASSERT(cond, do_abort)                                                 \
-    do {                                                                         \
-        if ((cond)) {                                                              \
-            _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,  \
+  do {                                                                         \
+    if ((cond)) {                                                              \
+      _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,  \
                     strlen(__func__), "SUCCESS", "%s", #cond);                 \
-        } else {                                                                   \
-            _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,  \
+    } else {                                                                   \
+      _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,  \
                     strlen(__func__), "FAILURE", "%s", #cond);                 \
-        }                                                                          \
-        if (!(cond) && (do_abort)) {                                               \
-            abort();                                                                 \
-        }                                                                          \
-    } while (0)
+    }                                                                          \
+    if (!(cond) && (do_abort)) {                                               \
+      abort();                                                                 \
+    }                                                                          \
+  } while (0)
 
 #define LOG(fmt, ...)                                                          \
-    do {                                                                         \
-        _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,    \
-                strlen(__func__), "LOG", fmt __VA_OPT__(, ) __VA_ARGS__);    \
-    } while (0)
+  do {                                                                         \
+    _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,    \
+                  strlen(__func__), "LOG", fmt __VA_OPT__(, ) __VA_ARGS__);    \
+  } while (0)
 
 #endif
 
 // not implemented (todo msg that aborts the program)
 #define NOT_IMPL(fmt, ...)                                                     \
-    do {                                                                         \
-        _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,    \
-                strlen(__func__), "NOT IMPL",                                \
-                fmt __VA_OPT__(, ) __VA_ARGS__);                             \
-        abort();                                                                   \
-    } while (0)
+  do {                                                                         \
+    _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,    \
+                  strlen(__func__), "NOT IMPL",                                \
+                  fmt __VA_OPT__(, ) __VA_ARGS__);                             \
+    abort();                                                                   \
+  } while (0)
 
 #define PANIC(fmt, ...)                                                        \
-    do {                                                                         \
-        _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,    \
-                strlen(__func__), "PANIC", fmt __VA_OPT__(, ) __VA_ARGS__);  \
-        abort();                                                                   \
-    } while (0)
+  do {                                                                         \
+    _log_template(__FILE_NAME__, strlen(__FILE_NAME__), __LINE__, __func__,    \
+                  strlen(__func__), "PANIC", fmt __VA_OPT__(, ) __VA_ARGS__);  \
+    abort();                                                                   \
+  } while (0)
 
 #endif // MAIN_H
